22eba7fecc9e627bd350ddf86d491e1ee3af9dd4
rterruso <rterruso813@gmail.com>
2024-04-21 13:33:58 -0400

    Routing put back in

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+
+// CREATE THE DIRECTORY OF PAGES
+const router = createBrowserRouter([
+  {
+  path:"home",
+  element: <Home/>
+  },
+  {
+    path: "about",
+    element: <About/>
+  },
+  {
+    path: "contact",
+    element: <Contact/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
