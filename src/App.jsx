c7c2372d17eadc7b4dd8905a2ef0b123080d80c8
gershomog <gershomog@gmail.com>
2024-04-24 17:18:33 -0400

    commited changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,3 @@
-// // IMPORTS HERE
-// import './main.css';
-// import './login.css';
-// import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import WelcomeMessage from './components/WelcomeMessage'
-// import SortFilterMenu from './components/SortFilterMenu'
-// import Login from '../src/pages/Login';
-// // import FetchAndDisplayFromQueue from './FetchAndDisplayFromQueue';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<><WelcomeMessage/><SortFilterMenu/></>} />
-//         <Route path="/login" element={<Login />} />
-//         {/* More routes can be added here */}
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 // IMPORTS HERE
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -29,9 +5,9 @@ import WelcomeMessage from './components/WelcomeMessage'
 import SortFilterMenu from './components/SortFilterMenu'
 import Login from '../src/pages/Login';
 import Register from '../src/pages/Register';
-// import FetchAndDisplayFromQueue from './FetchAndDisplayFromQueue';
 import './main.css';
 import './login.css';
+// import FetchAndDisplayFromQueue from './FetchAndDisplayFromQueue';
 
 function App () {
   return (
