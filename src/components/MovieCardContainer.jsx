9d20c98a1a00f866ec2ed813b36dfdc457721678
osman.elias@cscgfm.com <osmanelias45@gmail.com>
2024-05-05 01:25:34 -0400

    Styling changes
    Some changes to make the site look more presentable

--- src/components/MovieCardContainer.jsx
+++ src/components/MovieCardContainer.jsx
@@ -2,9 +2,9 @@ import MovieCard from "./MovieCard";
 
 function MovieCardContainer(){
     return(
-        <>
+        <div className="movie-grid">
         <MovieCard/>
-        </>
+        </div>
     )
 }
 
